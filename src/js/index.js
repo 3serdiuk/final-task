a85f65b47af85d2e0a7be6f708c215a452d47bd2
test <test@iMac-test.local>
2021-10-15 17:50:55 +0300

    Added header and a part on the banner block

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,15 @@
 import $ from 'jquery';
 
-$(document).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function () {
+    console.log("ready!");
+});
+
+$(".burger-menu").click(function () {
+    console.log($(".burger-menu"));
+    $(".burger-menu").toggleClass("open-menu");
+});
+
+$(".burger-menu").click(function () {
+    console.log($(".burger-menu"));
+    $(".header__nav").toggleClass("open-menu");
 });
