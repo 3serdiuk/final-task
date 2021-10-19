0e9da736c6026390992edd5a60e76a97505512d9
Oleksandr Serdiuk <oleksandrserdiuk3@gmail.com>
2021-10-19 15:14:58 +0300

    Added banner and footer sections

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,5 @@
 import $ from 'jquery';
 
-$(document).ready(function () {
-    console.log("ready!");
-});
-
 $(".burger-menu").click(function () {
     console.log($(".burger-menu"));
     $(".burger-menu").toggleClass("open-menu");
@@ -12,4 +8,9 @@ $(".burger-menu").click(function () {
 $(".burger-menu").click(function () {
     console.log($(".burger-menu"));
     $(".header__nav").toggleClass("open-menu");
+});
+
+$(".burger-menu").click(function () {
+    console.log($(".burger-menu"));
+    $("body").toggleClass("open-menu");
 });
